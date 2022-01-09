bcdd5fc4a62a246a199a3e826a45dac67bd25540
Guido <guidobedman@gmail.com>
2022-01-09 19:24:09 -0300

    Primer commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EncabezadoComponent } from './componentes/encabezado/encabezado.component';
+import { AcercaDeComponent } from './componentes/acerca-de/acerca-de.component';
+import { ExperienciaLaboralComponent } from './componentes/experiencia-laboral/experiencia-laboral.component';
+import { EducacionComponent } from './componentes/educacion/educacion.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EncabezadoComponent,
+    AcercaDeComponent,
+    ExperienciaLaboralComponent,
+    EducacionComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/componentes/experiencia-laboral/experiencia-laboral.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { PortfolioService } from 'src/app/servicios/portfolio.service';
+
+@Component({
+  selector: 'app-experiencia-laboral',
+  templateUrl: './experiencia-laboral.component.html',
+  styleUrls: ['./experiencia-laboral.component.css']
+})
+export class ExperienciaLaboralComponent implements OnInit {
+  experiencialaboralList:any;
+  constructor(private datosPortfolio:PortfolioService) { }
+
+  ngOnInit(): void {
+    this.datosPortfolio.obtenerDatos().subscribe(data =>{
+      this.experiencialaboralList=data.workexperience;
+    })
+  }
+
+}
