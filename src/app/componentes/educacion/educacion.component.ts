036a3e832a1b02fdade6d3a1212440d554939d95
Guido <guidobedman@gmail.com>
2022-03-19 18:45:19 -0300

    Conectado componente eduación con BBDD

--- src/app/componentes/educacion/educacion.component.ts
+++ src/app/componentes/educacion/educacion.component.ts
@@ -6,13 +6,32 @@ import { PortfolioService } from 'src/app/servicios/portfolio.service';
   templateUrl: './educacion.component.html',
   styleUrls: ['./educacion.component.css']
 })
-export class EducacionComponent implements OnInit {
+/*export class EducacionComponent implements OnInit {
   educacionList:any;
   constructor(private datosPortfolio:PortfolioService) { }
 
   ngOnInit(): void {
     this.datosPortfolio.obtenerDatos().subscribe(data =>{
       this.educacionList=data.education;
+    })
+  }
+
+} Version previa con JSON.*/
+
+export class EducacionComponent implements OnInit {
+  educacionLista:any[] = [{}];
+  constructor(private portfolioService:PortfolioService) { }
+
+  ngOnInit(): void {
+    this.obtenerEducacion();
+  }
+
+  obtenerEducacion(){
+    this.portfolioService.obtenerEducacion().subscribe(data => {
+      console.log(data);
+      this.educacionLista=data;
+    }, error => {
+      console.log(error)
     })
   }
 
--- src/app/servicios/portfolio.service.ts
+++ src/app/servicios/portfolio.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
-@Injectable({
+/*@Injectable({
   providedIn: 'root'
 })
 export class PortfolioService {
@@ -11,5 +11,22 @@ export class PortfolioService {
 
   obtenerDatos():Observable<any>{
     return this.http.get('./assets/data/data.json');
+  }
+} Version previa con JSON.*/
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PortfolioService {
+  URL = 'http://localhost:8080/api/';
+  constructor(private http:HttpClient) { }
+
+  obtenerDatos():Observable<any>{
+    return this.http.get('./assets/data/data.json');
+  }
+
+  obtenerEducacion():Observable<any>{
+    return this.http.get(this.URL+'educacion/lista');
   }
 }
